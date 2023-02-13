c40f5382b30bd5556c0d6b7c2ee14d8d7d24fe00
Bruna Flôr <brn.flor@gmail.com>
2023-02-13 16:28:23 +0000

    home passing on all tests

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,5 +1,5 @@
 import { createContext, ReactNode, useContext, useState } from "react";
-import { toast } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import { api } from "../services/api";
 import { Product, Stock } from "../types";
 
@@ -32,28 +32,34 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const addProduct = async (productId: number) => {
     try {
-      const responseProduct = await api.get(`/products/${productId}`);
-      const responseStock = await api.get(`/stock/${productId}`);
-      const storagedCart = localStorage.getItem("@RocketShoes:cart");
+      const responseProduct = await api
+        .get(`/products/${productId}`)
+        .catch((error) => {
+          throw new Error("Erro na adição do produto");
+        });
+      const responseStock = await api
+        .get(`/stock/${productId}`)
+        .catch((error) => {
+          throw new Error("Erro na adição do produto");
+        });
       const onCart = cart.findIndex((element) => element.id === productId);
+
       if (onCart === -1) {
         responseProduct.data["amount"] = 1;
         setCart((state) => [...state, responseProduct.data]);
         console.log("novo");
-        // localStorage.setItem(
-        //   "@RocketShoes:cart",
-        //   JSON.stringify([...cart, response.data])
-        // );
-      } else {
+      } else if (responseStock.data.amount > cart[onCart].amount) {
+        console.log("já tem desse");
         const newCart = [...cart];
         newCart[onCart].amount = newCart[onCart].amount + 1;
         setCart(newCart);
-        console.log("já tem desse");
+      } else {
+        throw new Error("Quantidade solicitada fora de estoque");
       }
-      // response.data["amount"] = 1;
-      // setCart((state) => [...state, response.data]);
-    } catch (err) {
-      console.log(err);
+
+      localStorage.setItem("@RocketShoes:cart", JSON.stringify([...cart]));
+    } catch (error) {
+      console.log(error);
     }
   };
   console.log("this is my cart", cart);
